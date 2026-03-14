77f3632554013315f1c78c660878441cde1ebd6d
Sara0921 <salahsara360@gmail.com>
2026-03-14 22:41:30 +0200

    Part01/Question01

--- Assignment 5/Program.cs
+++ Assignment 5/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            #region Part01
+            #region Question01
+            //>>An interface defines a contract that a class must follow. It specifies what a class can do, not how it does it.
+            //>>Depending directly on a concrete class tightly couples your code to a specific implementation, making it rigid and hard to change.
+            //>>Because Enable polymorphism without inheritance , Remove tight coupling between classes ,Enable multiple inheritance(behavior)
+
+            #endregion
+            #endregion
         }
     }
 }
